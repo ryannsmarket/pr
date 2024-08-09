musescore/MuseScore

Fix keyboard navigation order for dock panels
Old situation: you tell the docking system a navigation panel for each dock panel, and the docking system will mess with the order of this panel.

Problems: if a dock panel contains multiple navigation panels, you need to chain those other panels to the first one. And if the first panel is not really available, as in the case of the mixer toolbar, you can't tell the docking system about it.

New situation: you tell the docking system about the navigation section directly, instead of the navigation panel. The docking system will give you a `contentNavigationPanelOrderStart` property back, which you can use to correctly set the order of all navigation panels in your dock panel.

src/appshell/view/dockwindow/internal/dockbase.cpp
@@ -355,14 +355,24 @@ void DockBase::setFloating(bool floating)
     m_dockWidget->setFloating(floating);
 }
 
-void DockBase::setContentNavigationPanel(mu::ui::NavigationPanel* panel)
+void DockBase::setNavigationSection(ui::NavigationSection* section)
 {
-    if (m_contentNavigationPanel == panel) {
+    if (m_navigationSection == section) {
         return;
     }
 
-    m_contentNavigationPanel = panel;
-    emit contentNavigationPanelChanged();
+    m_navigationSection = section;
+    emit navigationSectionChanged();
+}
+
+void DockBase::setContentNavigationPanelOrderStart(int order)
+{
+    if (m_contentNavigationPanelOrderStart == order) {
+        return;
+    }
+
+    m_contentNavigationPanelOrderStart = order;
+    emit contentNavigationPanelOrderStartChanged();
 }
 
 void DockBase::init()
@@ -539,9 +549,14 @@ void DockBase::resize(int width, int height)
     applySizeConstraints();
 }
 
-mu::ui::NavigationPanel* DockBase::contentNavigationPanel() const
+mu::ui::NavigationSection* DockBase::navigationSection() const
 {
-    return m_contentNavigationPanel;
+    return m_navigationSection;
+}
+
+int DockBase::contentNavigationPanelOrderStart() const
+{
+    return m_contentNavigationPanelOrderStart;
 }
 
 void DockBase::componentComplete()

ryannsmarket/pr

src/framework/dockwindow/internal/dockbase.cpp
@@ -355,14 +355,24 @@ void DockBase::setFloating(bool floating)
     m_dockWidget->setFloating(floating);
 }
 
-void DockBase::setContentNavigationPanel(ui::NavigationPanel* panel)
+void DockBase::setNavigationSection(ui::NavigationSection* section)
 {
-    if (m_contentNavigationPanel == panel) {
+    if (m_navigationSection == section) {
         return;
     }
 
-    m_contentNavigationPanel = panel;
-    emit contentNavigationPanelChanged();
+    m_navigationSection = section;
+    emit navigationSectionChanged();
+}
+
+void DockBase::setContentNavigationPanelOrderStart(int order)
+{
+    if (m_contentNavigationPanelOrderStart == order) {
+        return;
+    }
+
+    m_contentNavigationPanelOrderStart = order;
+    emit contentNavigationPanelOrderStartChanged();
 }
 
 void DockBase::init()
@@ -539,9 +549,14 @@ void DockBase::resize(int width, int height)
     applySizeConstraints();
 }
 
-muse::ui::NavigationPanel* DockBase::contentNavigationPanel() const
+muse::ui::NavigationSection* DockBase::navigationSection() const
 {
-    return m_contentNavigationPanel;
+    return m_navigationSection;
+}
+
+int DockBase::contentNavigationPanelOrderStart() const
+{
+    return m_contentNavigationPanelOrderStart;
 }
 
 void DockBase::componentComplete()