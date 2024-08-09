musescore/MuseScore

Add the possibility to show toolbar items for dock panels

src/playback/qml/MuseScore/Playback/internal/MixerPanelToolbar.qml
@@ -1,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2023 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import QtQuick 2.15
+
+import MuseScore.Ui 1.0
+import MuseScore.UiComponents 1.0
+
+Item {
+    id: root
+
+    property alias navigation: navPanel
+
+    anchors.fill: parent
+
+    NavigationPanel {
+        id: navPanel
+        name: "MixerPanelToolbarPanel"
+        enabled: root.enabled && root.visible
+    }
+
+    FlatButton {
+        anchors.verticalCenter: parent.verticalCenter
+        anchors.right: parent.right
+        anchors.rightMargin: 2
+
+        icon: IconCode.SETTINGS_COG
+        text: qsTrc("playback", "Customize mixer")
+        orientation: Qt.Horizontal
+
+        navigation.panel: navPanel
+        navigation.row: 0
+    }
+}

ryannsmarket/pr

src/playback/qml/MuseScore/Playback/internal/MixerPanelToolbar.qml
@@ -1,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2023 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import QtQuick 2.15
+
+import Muse.Ui 1.0
+import Muse.UiComponents 1.0
+
+Item {
+    id: root
+
+    property alias navigation: navPanel
+
+    anchors.fill: parent
+
+    NavigationPanel {
+        id: navPanel
+        name: "MixerPanelToolbarPanel"
+        enabled: root.enabled && root.visible
+    }
+
+    FlatButton {
+        anchors.verticalCenter: parent.verticalCenter
+        anchors.right: parent.right
+        anchors.rightMargin: 2
+
+        icon: IconCode.SETTINGS_COG
+        text: qsTrc("playback", "Customize mixer")
+        orientation: Qt.Horizontal
+
+        navigation.panel: navPanel
+        navigation.row: 0
+    }
+}