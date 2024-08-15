musescore/MuseScore

hovering piano keyboard shows shadownote (for staves that are not tab or drum)

src/notation/inotationinteraction.h
@@ -45,8 +45,10 @@ public:
 
     // Shadow note
     virtual bool showShadowNote(const muse::PointF& pos) = 0;
+    virtual void showShadowNoteFromPianoKeyboard(const uint8_t note) = 0;
     virtual void hideShadowNote() = 0;
     virtual muse::RectF shadowNoteRect() const = 0;
+    virtual muse::async::Channel<bool> pianoKeyboardShadowNoteChanged() const = 0;
 
     // Visibility
     virtual void toggleVisible() = 0;

src/notation/internal/notationinteraction.cpp
@@ -78,6 +78,7 @@
 #include "engraving/dom/textedit.h"
 #include "engraving/dom/tuplet.h"
 #include "engraving/dom/undo.h"
+#include "engraving/dom/utils.h"
 #include "engraving/compat/dummyelement.h"
 
 #include "engraving/rw/xmlreader.h"
@@ -338,6 +339,65 @@ bool NotationInteraction::showShadowNote(const PointF& pos)
         return false;
     }
 
+    updateShadowNoteProperties(position);
+
+    return true;
+}
+
+void NotationInteraction::showShadowNoteFromPianoKeyboard(const uint8_t key)
+{
+    const mu::engraving::InputState& inputState = score()->inputState();
+    mu::engraving::ShadowNote& shadowNote = *score()->shadowNote();
+
+    Segment* inputSegment = inputState.segment();
+    if (!inputSegment) {
+        return;
+    }
+    staff_idx_t staffIdx = inputState.track() / VOICES;
+    const Staff* inputStaff = score()->staff(staffIdx);
+    const Fraction tick = inputSegment->tick();
+    const double mag     = inputStaff->staffMag(tick);
+    double lineDist = inputStaff->staffType(tick)->lineDistance().val()
+                      * (inputStaff->isTabStaff(tick) ? 1 : .5)
+                      * mag
+                      * score()->style().spatium();
+    int rLine = 0;
+    if (inputStaff->isDrumStaff(tick)) {
+        // different behavior? ignoring for now
+        hideShadowNote();
+        m_pianoKeyboardShadowNoteChanged.send(false);
+        return;
+    } else if (inputStaff->isTabStaff(tick)) {
+        // different behavior? ignoring for now
+        hideShadowNote();
+        m_pianoKeyboardShadowNoteChanged.send(false);
+        return;
+    } else {
+        int octave = key / 12;
+        int line = octave * 7 + mu::engraving::pitch2step(key);
+        ClefType clef = inputStaff->clef(tick);
+        rLine = mu::engraving::relStep(line, clef);
+
+        // temp
+        shadowNote.setStaffIdx(staffIdx);
+        shadowNote.setLineIndex(rLine);
+    }
+
+    double xPos = inputSegment->canvasPos().x();
+    double yPos = inputSegment->system()->staffCanvasYpage(staffIdx) + rLine * lineDist;
+
+    PointF p = { xPos, yPos };
+
+    Position position = { inputSegment, staffIdx, rLine, mu::engraving::INVALID_FRET_INDEX, p };
+    updateShadowNoteProperties(position);
+
+    m_pianoKeyboardShadowNoteChanged.send(true);
+}
+
+void NotationInteraction::updateShadowNoteProperties(Position& position)
+{
+    const mu::engraving::InputState& inputState = score()->inputState();
+    mu::engraving::ShadowNote& shadowNote = *score()->shadowNote();
     Staff* staff = score()->staff(position.staffIdx);
     const mu::engraving::Instrument* instr = staff->part()->instrument();
 
@@ -410,8 +470,6 @@ bool NotationInteraction::showShadowNote(const PointF& pos)
     score()->renderer()->layoutItem(&shadowNote);
 
     shadowNote.setPos(position.pos);
-
-    return true;
 }
 
 void NotationInteraction::hideShadowNote()
@@ -438,6 +496,11 @@ RectF NotationInteraction::shadowNoteRect() const
     rect.adjust(-penWidth, -penWidth, penWidth, penWidth);
 
     return rect;
+}
+
+muse::async::Channel<bool> NotationInteraction::pianoKeyboardShadowNoteChanged() const
+{
+    return m_pianoKeyboardShadowNoteChanged;
 }
 
 void NotationInteraction::toggleVisible()

src/notation/internal/notationinteraction.h
@@ -65,8 +65,10 @@ public:
 
     // Shadow note
     bool showShadowNote(const muse::PointF& pos) override;
+    void showShadowNoteFromPianoKeyboard(const uint8_t note) override;
     void hideShadowNote() override;
     muse::RectF shadowNoteRect() const override;
+    muse::async::Channel<bool> pianoKeyboardShadowNoteChanged() const override;
 
     // Visibility
     void toggleVisible() override;
@@ -393,6 +395,8 @@ private:
     template<typename P>
     void execute(void (mu::engraving::Score::* function)(P), P param);
 
+    void updateShadowNoteProperties(mu::engraving::Position& position);
+
     struct HitMeasureData
     {
         Measure* measure = nullptr;
@@ -443,6 +447,7 @@ private:
     DropData m_dropData;
     muse::async::Notification m_dropChanged;
 
+    muse::async::Channel<bool> m_pianoKeyboardShadowNoteChanged;
     muse::async::Channel<ScoreConfigType> m_scoreConfigChanged;
 
     engraving::BspTree m_droppableTree;

src/notation/tests/mocks/notationinteractionmock.h
@@ -33,8 +33,10 @@ public:
     MOCK_METHOD(INotationNoteInputPtr, noteInput, (), (const, override));
 
     MOCK_METHOD(bool, showShadowNote, (const muse::PointF&), (override));
+    MOCK_METHOD(void, showShadowNoteFromPianoKeyboard, (const uint8_t), (override));
     MOCK_METHOD(void, hideShadowNote, (), (override));
     MOCK_METHOD(muse::RectF, shadowNoteRect, (), (const, override));
+    MOCK_METHOD(muse::async::Channel<bool>, pianoKeyboardShadowNoteChanged, (), (const, override));
 
     MOCK_METHOD(void, toggleVisible, (), (override));
 

ryannsmarket/pr

src/notation/inotationinteraction.h
@@ -45,8 +45,10 @@ public:
 
     // Shadow note
     virtual bool showShadowNote(const muse::PointF& pos) = 0;
+    virtual void showShadowNoteForMidiPitch(const uint8_t note) = 0;
     virtual void hideShadowNote() = 0;
     virtual muse::RectF shadowNoteRect() const = 0;
+    virtual muse::async::Channel<bool> pianoKeyboardShadowNoteChanged() const = 0;
 
     // Visibility
     virtual void toggleVisible() = 0;

src/notation/internal/notationinteraction.cpp
@@ -78,6 +78,7 @@
 #include "engraving/dom/textedit.h"
 #include "engraving/dom/tuplet.h"
 #include "engraving/dom/undo.h"
+#include "engraving/dom/utils.h"
 #include "engraving/compat/dummyelement.h"
 
 #include "engraving/rw/xmlreader.h"
@@ -338,6 +339,63 @@ bool NotationInteraction::showShadowNote(const PointF& pos)
         return false;
     }
 
+    updateShadowNoteProperties(position);
+
+    return true;
+}
+
+void NotationInteraction::showShadowNoteForMidiPitch(const uint8_t key)
+{
+    const mu::engraving::InputState& inputState = score()->inputState();
+
+    if (!inputState.isValid()) {
+        return;
+    }
+
+    mu::engraving::ShadowNote& shadowNote = *score()->shadowNote();
+    Segment* inputSegment = inputState.segment();
+    staff_idx_t staffIdx = inputState.track() / VOICES;
+    const Staff* inputStaff = score()->staff(staffIdx);
+    const Fraction tick = inputSegment->tick();
+    const double mag     = inputStaff->staffMag(tick);
+    double lineDist = inputStaff->staffType(tick)->lineDistance().val()
+                      * (inputStaff->isTabStaff(tick) ? 1 : .5)
+                      * mag
+                      * score()->style().spatium();
+    int rLine = 0;
+    if (inputStaff->isDrumStaff(tick)) {
+        // different behavior? ignoring for now
+        hideShadowNote();
+        return;
+    } else if (inputStaff->isTabStaff(tick)) {
+        // different behavior? ignoring for now
+        hideShadowNote();
+        return;
+    } else {
+        int octave = key / 12;
+        int line = octave * 7 + mu::engraving::pitch2step(key);
+        ClefType clef = inputStaff->clef(tick);
+        rLine = mu::engraving::relStep(line, clef);
+
+        shadowNote.setStaffIdx(staffIdx);
+        shadowNote.setLineIndex(rLine);
+    }
+
+    double xPos = inputSegment->canvasPos().x();
+    double yPos = inputSegment->system()->staffCanvasYpage(staffIdx) + rLine * lineDist;
+
+    PointF p = { xPos, yPos };
+
+    Position position = { inputSegment, staffIdx, rLine, mu::engraving::INVALID_FRET_INDEX, p };
+    updateShadowNoteProperties(position);
+
+    m_pianoKeyboardShadowNoteChanged.send(true);
+}
+
+void NotationInteraction::updateShadowNoteProperties(Position& position)
+{
+    const mu::engraving::InputState& inputState = score()->inputState();
+    mu::engraving::ShadowNote& shadowNote = *score()->shadowNote();
     Staff* staff = score()->staff(position.staffIdx);
     const mu::engraving::Instrument* instr = staff->part()->instrument();
 
@@ -410,13 +468,12 @@ bool NotationInteraction::showShadowNote(const PointF& pos)
     score()->renderer()->layoutItem(&shadowNote);
 
     shadowNote.setPos(position.pos);
-
-    return true;
 }
 
 void NotationInteraction::hideShadowNote()
 {
     score()->shadowNote()->setVisible(false);
+    m_pianoKeyboardShadowNoteChanged.send(false);
 }
 
 RectF NotationInteraction::shadowNoteRect() const
@@ -438,6 +495,11 @@ RectF NotationInteraction::shadowNoteRect() const
     rect.adjust(-penWidth, -penWidth, penWidth, penWidth);
 
     return rect;
+}
+
+muse::async::Channel<bool> NotationInteraction::pianoKeyboardShadowNoteChanged() const
+{
+    return m_pianoKeyboardShadowNoteChanged;
 }
 
 void NotationInteraction::toggleVisible()

src/notation/internal/notationinteraction.h
@@ -65,8 +65,10 @@ public:
 
     // Shadow note
     bool showShadowNote(const muse::PointF& pos) override;
+    void showShadowNoteForMidiPitch(const uint8_t note) override;
     void hideShadowNote() override;
     muse::RectF shadowNoteRect() const override;
+    muse::async::Channel<bool> pianoKeyboardShadowNoteChanged() const override;
 
     // Visibility
     void toggleVisible() override;
@@ -395,6 +397,8 @@ private:
     template<typename P>
     void execute(void (mu::engraving::Score::* function)(P), P param);
 
+    void updateShadowNoteProperties(mu::engraving::Position& position);
+
     struct HitMeasureData
     {
         Measure* measure = nullptr;
@@ -445,6 +449,7 @@ private:
     DropData m_dropData;
     muse::async::Notification m_dropChanged;
 
+    muse::async::Channel<bool> m_pianoKeyboardShadowNoteChanged;
     muse::async::Channel<ScoreConfigType> m_scoreConfigChanged;
 
     engraving::BspTree m_droppableTree;

src/notation/tests/mocks/notationinteractionmock.h
@@ -33,8 +33,10 @@ public:
     MOCK_METHOD(INotationNoteInputPtr, noteInput, (), (const, override));
 
     MOCK_METHOD(bool, showShadowNote, (const muse::PointF&), (override));
+    MOCK_METHOD(void, showShadowNoteForMidiPitch, (const uint8_t), (override));
     MOCK_METHOD(void, hideShadowNote, (), (override));
     MOCK_METHOD(muse::RectF, shadowNoteRect, (), (const, override));
+    MOCK_METHOD(muse::async::Channel<bool>, pianoKeyboardShadowNoteChanged, (), (const, override));
 
     MOCK_METHOD(void, toggleVisible, (), (override));
 