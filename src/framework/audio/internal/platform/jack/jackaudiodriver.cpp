musescore/MuseScore

route midi-in-devices from midi-inport to jack-audio

src/framework/audio/internal/platform/jack/jackaudiodriver.cpp
@@ -21,6 +21,7 @@
  */
 #include "jackaudiodriver.h"
 #include <jack/midiport.h>
+#include "framework/midi/miditypes.h"
 #include "framework/midi/midierrors.h"
 
 #include <fcntl.h>
@@ -41,6 +42,10 @@
 using namespace mu::audio;
 using namespace mu::midi;
 namespace mu::audio {
+/*
+ * MIDI
+ */
+
 mu::Ret sendEvent_noteonoff(void* pb, int framePos, const mu::midi::Event& e)
 {
     unsigned char* p = jack_midi_event_reserve(pb, framePos, 3);
@@ -119,6 +124,10 @@ mu::Ret sendEvent(const Event& e, void* pb)
 
     return Ret(true);
 }
+
+/*
+ * AUDIO
+ */
 
 static int jack_process_callback(jack_nframes_t nframes, void* args)
 {
@@ -266,7 +275,6 @@ bool JackDriverState::open(const IAudioDriver::Spec& spec, IAudioDriver::Spec* a
     }
 
     // midi
-    jack_options_t options = (jack_options_t)0;
     int portFlag = JackPortIsOutput;
     const char* portType = JACK_DEFAULT_MIDI_TYPE;
     jack_port_t* port = jack_port_register(handle, "Musescore", portType, portFlag, 0);
@@ -288,8 +296,39 @@ bool JackDriverState::isOpened() const
     return m_jackDeviceHandle != nullptr;
 }
 
+/*
+ * MIDI
+ */
+
 bool JackDriverState::pushMidiEvent(mu::midi::Event& e)
 {
     m_midiQueue.push(e);
     return true;
+}
+
+std::vector<mu::midi::MidiDevice> JackDriverState::availableMidiDevices() const
+{
+    std::vector<mu::midi::MidiDevice> ports;
+    std::vector<mu::midi::MidiDevice> ret;
+    jack_client_t* client = static_cast<jack_client_t*>(m_jackDeviceHandle);
+    const char** prts = jack_get_ports(client, 0, 0, 0);
+    int devIndex = 0;
+    for (const char** p = prts; p && *p; ++p) {
+        jack_port_t* port = jack_port_by_name(client, *p);
+        int flags = jack_port_flags(port);
+        if (!(flags & JackPortIsInput)) {
+            continue;
+        }
+        char buffer[128];
+        strncpy(buffer, *p, sizeof(buffer) - 1);
+        buffer[sizeof(buffer) - 1] = 0;
+        if (strncmp(buffer, "MuseScore", 9) == 0) {
+            continue;
+        }
+        mu::midi::MidiDevice dev;
+        dev.name = buffer;
+        dev.id = makeUniqueDeviceId(devIndex++, 0, 0);
+        ports.push_back(std::move(dev));
+    }
+    return ports;
 }

src/framework/audio/internal/platform/osx/osxaudiodriver.mm
@@ -530,3 +530,9 @@ bool OSXAudioDriver::pushMidiEvent(mu::midi::Event&)
 {
     return true; // dummy
 }
+
+std::vector<mu::midi::MidiDevice> OSXAudioDriver::availableMidiDevices() const
+{
+    std::vector<mu::midi::MidiDevice> x;
+    return x; // dummy
+}

src/framework/audio/internal/platform/win/wasapiaudiodriver.cpp
@@ -312,3 +312,9 @@ bool WasapiAudioDriver::pushMidiEvent(mu::midi::Event&)
 {
     return true;
 }
+
+std::vector<mu::midi::MidiDevice> WasapiAudioDriver::availableMidiDevices() const
+{
+    std::vector<mu::midi::MidiDevice> x;
+    return x; // dummy
+}

src/framework/audio/internal/platform/win/wasapiaudiodriver.h
@@ -52,6 +52,8 @@ public:
     async::Notification outputDeviceBufferSizeChanged() const override;
     std::vector<unsigned int> availableOutputDeviceBufferSizes() const override;
     bool pushMidiEvent(mu::midi::Event& e) override;
+    std::vector<mu::midi::MidiDevice> availableMidiDevices() const override;
+
     void resume() override;
     void suspend() override;
 

src/stubs/framework/audio/audiodriverstub.h
@@ -43,6 +43,7 @@ public:
     AudioDeviceList availableOutputDevices() const override;
     async::Notification availableOutputDevicesChanged() const override;
     bool pushMidiEvent(mu::midi::Event& e) override;
+    std::vector<mu::midi::MidiDevice> availableMidiDevices() const override;
 
     unsigned int outputDeviceBufferSize() const override;
     bool setOutputDeviceBufferSize(unsigned int bufferSize) override;

ryannsmarket/pr

src/framework/audio/internal/platform/jack/jackaudiodriver.cpp
@@ -21,6 +21,7 @@
  */
 #include "jackaudiodriver.h"
 #include <jack/midiport.h>
+#include "framework/midi/miditypes.h"
 #include "framework/midi/midierrors.h"
 
 #include <fcntl.h>
@@ -41,6 +42,10 @@
 using namespace muse::audio;
 using namespace muse::midi;
 namespace muse::audio {
+/*
+ * MIDI
+ */
+
 muse::Ret sendEvent_noteonoff(void* pb, int framePos, const muse::midi::Event& e)
 {
     unsigned char* p = jack_midi_event_reserve(pb, framePos, 3);
@@ -119,6 +124,10 @@ muse::Ret sendEvent(const Event& e, void* pb)
 
     return Ret(true);
 }
+
+/*
+ * AUDIO
+ */
 
 static int jack_process_callback(jack_nframes_t nframes, void* args)
 {
@@ -266,7 +275,6 @@ bool JackDriverState::open(const IAudioDriver::Spec& spec, IAudioDriver::Spec* a
     }
 
     // midi
-    jack_options_t options = (jack_options_t)0;
     int portFlag = JackPortIsOutput;
     const char* portType = JACK_DEFAULT_MIDI_TYPE;
     jack_port_t* port = jack_port_register(handle, "Musescore", portType, portFlag, 0);
@@ -288,8 +296,45 @@ bool JackDriverState::isOpened() const
     return m_jackDeviceHandle != nullptr;
 }
 
+/*
+ * MIDI
+ */
+
 bool JackDriverState::pushMidiEvent(muse::midi::Event& e)
 {
     m_midiQueue.push(e);
     return true;
+}
+
+std::vector<muse::midi::MidiDevice> JackDriverState::availableMidiDevices() const
+{
+    std::vector<muse::midi::MidiDevice> ports;
+    std::vector<muse::midi::MidiDevice> ret;
+    jack_client_t* client = static_cast<jack_client_t*>(m_jackDeviceHandle);
+    const char** prts = jack_get_ports(client, 0, 0, 0);
+    if (!prts) {
+        return ports;
+    }
+    int devIndex = 0;
+    for (const char** p = prts; p && *p; ++p) {
+        jack_port_t* port = jack_port_by_name(client, *p);
+        int flags = jack_port_flags(port);
+        if (!(flags & JackPortIsInput)) {
+            continue;
+        }
+        char buffer[128];
+        strncpy(buffer, *p, sizeof(buffer) - 1);
+        buffer[sizeof(buffer) - 1] = 0;
+        if (strncmp(buffer, "MuseScore", 9) == 0) {
+            continue;
+        }
+        muse::midi::MidiDevice dev;
+        dev.name = buffer;
+        dev.id = makeUniqueDeviceId(devIndex++, 0, 0);
+        ports.push_back(std::move(dev));
+    }
+
+    free(prts);
+
+    return ports;
 }

src/framework/audio/internal/platform/osx/osxaudiodriver.mm
@@ -581,3 +581,9 @@ bool OSXAudioDriver::pushMidiEvent(muse::midi::Event&)
 {
     return true; // dummy
 }
+
+std::vector<muse::midi::MidiDevice> OSXAudioDriver::availableMidiDevices() const
+{
+    std::vector<muse::midi::MidiDevice> x;
+    return x; // dummy
+}

src/framework/audio/internal/platform/win/wasapiaudiodriver.cpp
@@ -374,3 +374,9 @@ bool WasapiAudioDriver::pushMidiEvent(muse::midi::Event&)
 {
     return true;
 }
+
+std::vector<muse::midi::MidiDevice> WasapiAudioDriver::availableMidiDevices() const
+{
+    std::vector<muse::midi::MidiDevice> x;
+    return x; // dummy
+}

src/framework/audio/internal/platform/win/wasapiaudiodriver.h
@@ -65,6 +65,8 @@ public:
     std::vector<unsigned int> availableOutputDeviceSampleRates() const override;
 
     bool pushMidiEvent(muse::midi::Event& e) override;
+    std::vector<muse::midi::MidiDevice> availableMidiDevices() const override;
+
     void resume() override;
     void suspend() override;
 

src/framework/stubs/audio/audiodriverstub.h
@@ -45,6 +45,7 @@ public:
     AudioDeviceList availableOutputDevices() const override;
     async::Notification availableOutputDevicesChanged() const override;
     bool pushMidiEvent(muse::midi::Event& e) override;
+    std::vector<muse::midi::MidiDevice> availableMidiDevices() const override;
 
     unsigned int outputDeviceBufferSize() const override;
     bool setOutputDeviceBufferSize(unsigned int bufferSize) override;