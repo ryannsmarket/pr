musescore/MuseScore

GUI filter musescore-ingress midiport

src/framework/audio/internal/platform/alsa/alsaaudiodriver.cpp
@@ -201,7 +201,7 @@ void AlsaDriverState::registerMidiInputQueue(async::Channel<mu::midi::tick_t, mu
     m_eventReceived = midiInputQueue;
 }
 
-std::vector<mu::midi::MidiDevice> AlsaDriverState::availableMidiDevices() const
+std::vector<mu::midi::MidiDevice> AlsaDriverState::availableMidiDevices(mu::midi::MidiPortDirection direction) const
 {
     std::vector<mu::midi::MidiDevice> x;
     return x; // dummy

ryannsmarket/pr

src/framework/audio/internal/platform/alsa/alsaaudiodriver.cpp
@@ -201,7 +201,7 @@ void AlsaDriverState::registerMidiInputQueue(async::Channel<muse::midi::tick_t, 
     m_eventReceived = midiInputQueue;
 }
 
-std::vector<muse::midi::MidiDevice> AlsaDriverState::availableMidiDevices() const
+std::vector<muse::midi::MidiDevice> AlsaDriverState::availableMidiDevices(muse::midi::MidiPortDirection direction) const
 {
     std::vector<muse::midi::MidiDevice> x;
     return x; // dummy